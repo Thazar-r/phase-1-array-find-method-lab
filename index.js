2f41235d2028263788ae00f276cb8e48f34629fb
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-07-02 10:26:46 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(records) {
+    const winningYearObj = records.find(record => record.result === "W");
+    if (winningYearObj) {
+      return winningYearObj.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  
